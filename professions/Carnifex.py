73f5608e58c441de4ae7dce5f0521a268a8255d0
Joe Granville <jgranville@riotgames.com>
2015-04-19 22:19:40 -0700

    Added JSON loading support for profession details

--- professions/Carnifex.py
+++ professions/Carnifex.py
@@ -1,7 +1,11 @@
 import Profession
+import json
 
 def Carnifex():
-    carnifex_ability = None
-    carnifex_dict = {'profession_ability': carnifex_ability, 'basic_attack_melee_delta': 2, 'basic_attack_ranged_delta': 0, 'movement_delta': 5, 'hit_points': 35, 'initiative_token_delta': 2, 'name': 'Carnifex'}
-    carnifex_profession = Profession.Profession(carnifex_dict)
-    return carnifex_profession
+    
+    with open('professions/carnifex.json') as data_file:
+        #carnifex_dict = {'profession_ability': carnifex_ability, 'basic_attack_melee_delta': 2, 'basic_attack_ranged_delta': 0, 'movement_delta': 5, 'hit_points': 35, 'initiative_token_delta': 2, 'name': 'Carnifex'}
+        carnifex_dict = json.load(data_file)
+        carnifex_dict['profession_ability'] = None
+        carnifex_profession = Profession.Profession(carnifex_dict)
+        return carnifex_profession
--- professions/Centurion.py
+++ professions/Centurion.py
@@ -1,5 +1,6 @@
 import Profession
 import DamageDealtEvent 
+import json
 
 def centurionAbility(self, event):
     if type(event) is DamageDealtEvent:
@@ -8,7 +9,10 @@ def centurionAbility(self, event):
 
 #I know doing this makes me a  dummy. gotta go fast
 def Centurion():
-    centurion_ability = centurionAbility
-    centurion_dict = {'profession_ability': centurion_ability, 'basic_attack_melee_delta': 0, 'basic_attack_ranged_delta': 0, 'movement_delta': 4, 'hit_points': 45, 'initiative_token_delta': 1, 'name': 'Centurion'}
-    centurion_profession = Profession.Profession(centurion_dict)
+   # centurion_dict = {'profession_ability': centurion_ability, 'basic_attack_melee_delta': 0, 'basic_attack_ranged_delta': 0, 'movement_delta': 4, 'hit_points': 45, 'initiative_token_delta': 1, 'name': 'Centurion'}
+    centurion_profession = None
+    with open('professions/centurion.json', 'r') as data_file:    
+        centurion_dict = json.load(data_file)
+        centurion_ability = centurion_dict['profession_ability'] = centurionAbility
+        centurion_profession = Profession.Profession(centurion_dict)
     return centurion_profession
